f01a60a91b1e649c7da445017d01631ad127b3e0
JItesh <suvarnajitesh313@gmail.com>
2018-04-14 18:59:32 +0530

    RecyclerView layout animation and launcher

--- app/src/main/java/inc/yoman/matchescard/ui/activity/MainActivity.kt
+++ app/src/main/java/inc/yoman/matchescard/ui/activity/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.view.View
+import android.view.animation.AnimationUtils
+import android.view.animation.LayoutAnimationController
 import com.google.gson.Gson
 import inc.yoman.matchescard.R
 import inc.yoman.matchescard.model.MatchesProfileModel
 import inc.yoman.matchescard.ui.adapter.MatchesProfileAdapter
 import inc.yoman.matchescard.utils.AppUtils
 import inc.yoman.matchescard.utils.Constants
+import inc.yoman.matchescard.utils.customview.ItemOffsetDecoration
 import io.reactivex.Flowable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
@@ -51,14 +54,31 @@ class MainActivity : AppCompatActivity() {
             return@fromCallable response?.body()?.string()
         }
                 .subscribeOn(Schedulers.io())
-                .map { result -> mappingResult(result) }
+                .map { resultMap -> mappingResult(resultMap) }
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({ result ->
-                    progress_bar.visibility = View.GONE
-                        val adapterMatchesProfile = MatchesProfileAdapter(this,result)
-                        recyclerView_matches_card.adapter = adapterMatchesProfile
+                .subscribe({ it ->
+                    it?.let {
+                        progress_bar.visibility = View.GONE
+                        populateRecyclerView(it)
+                    }
                 }, { e -> AppUtils.logMe("Api Call error", e.toString())
                 })
+    }
+
+    private fun populateRecyclerView(result: ArrayList<MatchesProfileModel>) {
+        val adapterMatchesProfile = MatchesProfileAdapter(this,result)
+        recyclerView_matches_card.adapter = adapterMatchesProfile
+
+        val spacing = resources.getDimensionPixelOffset(R.dimen.default_spacing_small)
+        recyclerView_matches_card.addItemDecoration(ItemOffsetDecoration(spacing))
+
+        val context = recyclerView_matches_card.context
+
+        val controller: LayoutAnimationController = AnimationUtils.loadLayoutAnimation(context,  R.anim.layout_animation_from_bottom)
+
+        recyclerView_matches_card.layoutAnimation = controller
+        recyclerView_matches_card.adapter.notifyDataSetChanged()
+        recyclerView_matches_card.scheduleLayoutAnimation()
     }
 
     private fun mappingResult(result: Any): ArrayList<MatchesProfileModel> {
--- app/src/main/java/inc/yoman/matchescard/ui/adapter/MatchesProfileAdapter.kt
+++ app/src/main/java/inc/yoman/matchescard/ui/adapter/MatchesProfileAdapter.kt
@@ -5,7 +5,6 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import inc.yoman.matchescard.R
 import inc.yoman.matchescard.model.MatchesProfileModel
 import inc.yoman.matchescard.utils.AppUtils
 import kotlinx.android.synthetic.main.item_matches_profile.view.*
@@ -26,21 +25,17 @@ class MatchesProfileAdapter(activity: Activity, data: List<MatchesProfileModel>)
 
     override fun onBindViewHolder(holder: MatchesProfileViewHolder, position: Int) = holder.bind(activity, data[position])
 
-//    fun swapData(data: List<MatchesProfileModel>) {
-//        this.data = data
-//        notifyDataSetChanged()
-//    }
-
     class MatchesProfileViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(activity: Activity, item: MatchesProfileModel) = with(itemView) {
 
-            itemView.textView_username.text = (item.name.title).capitalize() + " " + (item.name.first).capitalize() + " " + (item.name.last).capitalize()
+            var userDetails: String  = (item.name.title).capitalize() + " " + (item.name.first).capitalize() + " " + (item.name.last).capitalize()
+
+            itemView.textView_user_details.text = userDetails
             itemView.textView_address.text = (item.location.city).capitalize() + ", " + (item.location.state).capitalize()
 
            AppUtils.loadImageInImageView(item.picture.large,imageView_profile, item.gender)
 
             setOnClickListener {
-
                 it.imageView_call.setOnClickListener { AppUtils.callIntent(activity, item.phone) }
                 it.imageView_email.setOnClickListener { AppUtils.sendEmail(activity, item.email) }
             }
